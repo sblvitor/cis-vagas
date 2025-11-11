198f589bb4c9b96fb53440713c56cdd563fdde05
Vitor <sblvitor@gmail.com>
2025-11-10 23:33:42 -0400

    add and config uniwind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { ConvexProvider, ConvexReactClient } from 'convex/react';
 import { Stack } from "expo-router";
+import '../global.css';
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
     unsavedChangesWarning: false,
